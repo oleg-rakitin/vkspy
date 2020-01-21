cbf0a09cba83ad21a6b653caf3cfc871780331ea
Pavel Sokolov <pavel.sokolov@waveaccess.ru>
2020-01-21 13:07:40 +0300

    Updates

---
+++ precache-manifest.de19c5dd6103b0e931c0c37d357e37e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b5b75268d90a5e06bc7a74443119ee",
+    "url": "/vkspy/index.html"
+  },
+  {
+    "revision": "8c4c65f8673d30304092",
+    "url": "/vkspy/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "8949af9ddeba8ec660b0",
+    "url": "/vkspy/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8c4c65f8673d30304092",
+    "url": "/vkspy/static/js/2.dc1b987c.chunk.js"
+  },
+  {
+    "revision": "8949af9ddeba8ec660b0",
+    "url": "/vkspy/static/js/main.540cab3c.chunk.js"
+  },
+  {
+    "revision": "59e7c840b25f7e0fb8c1",
+    "url": "/vkspy/static/js/runtime-main.b0238352.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkspy/static/media/persik.4e1ec840.png"
+  }
+]);
